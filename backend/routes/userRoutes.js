a7eb6faf5eca693c5840d710d1d070a95594705a
Shreyas Kulkarni <shreyaskulkarni489@gmail.com>
2026-06-10 20:17:43 +0530

    Day 5: Added bcrypt password hashing and secure registration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const router = express.Router();
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
     const user = new User({
       name,
       email,
-      password,
+      password: hashedPassword,
       role,
     });
 
     await user.save();
 
     res.status(201).json({
-      message: "User Registered Successfully",
-      user,
-    });
+  message: "User Registered Successfully",
+  user: {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  },
+});
   } catch (error) {
     res.status(500).json({
       message: error.message,
@@ -27,4 +46,56 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// LOGIN ROUTE
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login Successful",
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 module.exports = router;
